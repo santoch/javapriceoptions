734352342c96da17ac17c97e18f005cb345ef682
santoch <santoch2020@outlook.com>
2020-06-26 18:39:14 -0700

    Add IGreeks interface

---
+++ src/main/java/com/santoch/optionpricing/common/IGreeks.java
@@ -0,0 +1,49 @@
+package com.santoch.optionpricing.common;
+
+import java.time.ZonedDateTime;
+
+public interface IGreeks {
+    double getDelta();
+
+    void setDelta(double delta);
+
+    double getGamma();
+
+    void setGamma(double gamma);
+
+    double getTheta();
+
+    void setTheta(double theta);
+
+    double getVega();
+
+    void setVega(double vega);
+
+    double getRho();
+
+    void setRho(double rho);
+
+    double getPhi();
+
+    void setPhi(double phi);
+
+    double getBidIv();
+
+    void setBidIv(double bid_iv);
+
+    double getMidIv();
+
+    void setMidIv(double mid_iv);
+
+    double getAskIv();
+
+    void setAskIv(double ask_iv);
+
+    double getSmvVol();
+
+    void setSmvVol(double smv_vol);
+
+    ZonedDateTime getUpdate_time();
+
+    void setUpdate_time(ZonedDateTime update_time);
+}
